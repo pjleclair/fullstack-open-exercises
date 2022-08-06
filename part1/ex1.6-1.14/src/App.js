a00a32e63257ffdcee618747193f68a97b3dddf8
lompdoctor <pleclair@protonmail.com>
2022-08-06 13:58:30 -0500

    ex1.6 complete

--- part1/ex1.6-1.14/src/App.js
+++ part1/ex1.6-1.14/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>Give Feedback</h1>
+  )
 }
 
-export default App;
+const RatingButtons = (props) => {
+  return (
+    <div>
+      <button id="button--good" onClick={props.onClick}>good</button>
+      <button id="button--neutral" onClick={props.onClick}>neutral</button>
+      <button id="button--bad" onClick={props.onClick}>bad</button>
+    </div>
+  )
+}
+
+const NumberCounter = (props) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <div>
+        <div>good: {props.numGood}</div>
+        <div>neutral: {props.numNeutral}</div>
+        <div>bad: {props.numBad}</div>
+      </div>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (event) => {
+    console.log(event.target.id)
+    const {id: buttonName} = event.target
+    console.log(buttonName)
+    if (buttonName === "button--good") {
+      setGood(prevCount => prevCount+1)
+    } else if (buttonName === "button--neutral") {
+      setNeutral(prevCount => prevCount+1)
+    } else {
+      setBad(prevCount => prevCount+1)
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <RatingButtons 
+        onClick={handleClick}
+      />
+      <NumberCounter
+        numGood={good}
+        numNeutral={neutral}
+        numBad={bad}
+      />
+    </div>
+  )
+}
+
+export default App
