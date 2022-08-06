271ef81ee28c1a380bf45f963fcbc1ccf0ae4b6c
lompdoctor <pleclair@protonmail.com>
2022-08-06 17:53:31 -0500

    complete ex2.1

--- part2/ex2.1-2.5/src/App.js
+++ part2/ex2.1-2.5/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Course = ({course}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Course List</h1>
+      <ul>
+      {course.parts.map(part =>
+        <li>{part.name} {part.exercises}</li>
+      )}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
