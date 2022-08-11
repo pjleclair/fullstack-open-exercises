8bb031c5a378160656e1ea0a45172ba98ec280e8
lompdoctor <pleclair@protonmail.com>
2022-08-11 13:07:59 -0500

    complete ex2.15

---
+++ part2/ex2.15-2.18/src/App.js
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const SearchFunction = (props) => {
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <div>
+          search the listings: <input onInput={props.onInput}/>
+        </div>
+    </div>
+  )
+}
+
+const AddEntry = (props) => {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <h1>add an entry:</h1>
+      <div>
+        name: <input id='name' onInput={props.onInput}/>
+      </div>
+      <div>
+        number: <input id='number' onInput={props.onInput}/>
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const Numbers = (props) => {
+  const newArray = props.numArray.map(num => 
+    <div key={num.name}>{num.name}: {num.num}</div>
+  )
+  return (
+    <div>
+     <h1>Numbers</h1>
+      {props.searchFilters !== '' ? newArray.filter(name => name.key.toLowerCase().includes(props.searchFilters.toLowerCase())) : newArray}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+  const dataHook = () => {
+    axios
+      .get('http://localhost:3001/data')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  }
+
+  React.useEffect(dataHook, [])
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    persons.find(name => 
+      name.name === newName
+    ) !== undefined ? alert(`${newName} is already in the phonebook!`) :
+    setPersons(prevState => [
+      ...prevState,
+      {name: newName, num: newNum}
+    ])
+    const newPerson = {name: newName, num: newNum}
+    axios
+      .post('http://localhost:3001/data', newPerson)
+      .then(response => {
+        console.log(response)
+      })
+  }
+
+  const handleInput = (event) => {
+    event.target.id === 'name' ? setNewName(event.target.value) : setNewNum(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
+
+  return (
+    <div>
+      <SearchFunction 
+        onInput={handleSearch}
+      />
+      <AddEntry 
+        onInput={handleInput}
+        onSubmit={handleSubmit}
+      /> 
+      <Numbers 
+        numArray={persons}
+        searchFilters={newSearch}
+      />
+    </div>
+  )
+}
+
+export default App
