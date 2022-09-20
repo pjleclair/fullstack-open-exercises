418365736c79e3bb3a64aff99383ef679ab80642
lompdoctor <pleclair@protonmail.com>
2022-09-20 14:41:48 -0500

    Implement proxy address

--- part2/ex2.15-2.18/src/services/entries.js
+++ part2/ex2.15-2.18/src/services/entries.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://agile-sierra-56026.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     return axios.get(baseUrl)
